6d7c84cea12939be9312510f5c89699d5a32148e
huseyinsimsekk <hsynsimsek723@gmail.com>
2020-10-22 17:48:25 +0300

    Repository pattern will add

---
+++ Contracts/IRepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Contracts
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        IQueryable<T> FindAll();
+        IQueryable<T> FindByContition(Expression<Func<T, bool>> expression);
+        void Create(T model);
+        void Update(T model);
+        void Delete(T model);
+    }
+}
