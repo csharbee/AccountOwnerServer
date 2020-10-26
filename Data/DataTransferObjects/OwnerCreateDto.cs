9d9793bba581492580e20fce8711ff5df72fddd3
huseyinsimsekk <hsynsimsek723@gmail.com>
2020-10-26 17:21:16 +0300

    DataTransferObjects folder added
    DTOs are moved to this folder

--- Data/DataTransferObjects/OwnerCreateDto.cs
+++ Data/DataTransferObjects/OwnerCreateDto.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
-namespace Data.ViewModels
+namespace Data.DataTransferObjects
 {
-    public class OwnerCreateDTO
+    public class OwnerCreateDto
     {
         [Required(ErrorMessage = "Name is required")]
         [StringLength(60, ErrorMessage = "Name can't be longer than 60 characters")]
